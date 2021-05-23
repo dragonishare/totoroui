378d5c5feeeec428523ea9d64e890822b63ea321
dragonishare <dragonishare@gmail.com>
2021-05-23 21:36:06 +0800

    示例添加注释

--- website/main.js
+++ website/main.js
@@ -1,8 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+// 导入组件库
 import TotoroUI from "../packages/index.js";
-
+// 注册组件库
 Vue.use(TotoroUI);
 
 Vue.config.productionTip = false;
